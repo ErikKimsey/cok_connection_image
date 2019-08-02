2554314a7e0113a4c6ac05d3398ea426b5dcebf8
Erik Kimsey <kimseyerik@gmail.com>
2019-08-02 15:23:59 -0400

    Pcking image from device seems to be working

--- App.js
+++ App.js
@@ -36,7 +36,7 @@ import ConnectionImage from './components/ImagePicker';
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
+			<Text>Add image:</Text>
 			<ConnectionImage />
 		</View>
 	);
--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
+import GLOBAL_STYLES from '../styles/global_styles';
 const grunge = require('../assets/grunge_chicks.jpeg');
-const add = require('../assets/add.png');
+const add = require('../assets/add-user.png');
 
 export default class ConnectionImage extends Component {
 	constructor(props) {
@@ -41,14 +42,26 @@ export default class ConnectionImage extends Component {
 		const { image } = this.state;
 		console.log('iamge');
 		console.log(image);
-		return (
-			<TouchableOpacity style={styles.btn} onPress={this._pickImage}>
-				<View style={styles.absoluteView}>
-					<Text style={styles.addText}>+</Text>
-				</View>
-				{image && <Image source={{ uri: image }} style={styles.img} />}
-			</TouchableOpacity>
-		);
+		if (image) {
+			return (
+				<TouchableOpacity style={styles.btn} onPress={this._pickImage}>
+					<View style={styles.absoluteView}>
+						<Text style={styles.addText}>+</Text>
+					</View>
+					{image && <Image source={{ uri: image }} style={styles.img} />}
+				</TouchableOpacity>
+			);
+		} else {
+			return (
+				<TouchableOpacity style={styles.btn} onPress={this._pickImage}>
+					<View style={styles.absoluteView}>
+						{/* <Text style={styles.addText}>+</Text> */}
+						<Image source={add} style={[ styles.img, { width: 100, height: 100, opacity:0.4 } ]} resizeMode="cover" />
+					</View>
+					{/* {image && <Image source={{ uri: add }} style={styles.img} />} */}
+				</TouchableOpacity>
+			);
+		}
 	}
 }
 
@@ -61,16 +74,22 @@ const styles = StyleSheet.create({
 	},
 	addText: {
 		fontSize: 92,
-		textAlign: 'center'
+		textAlign: 'center',
+		color: GLOBAL_STYLES.highlightBlue
 	},
 	img: {
 		width: 200,
-		height: 200
-		// backgroundColor: '#5d0000'
+		height: 200,
+		borderRadius: 30
 	},
 	btn: {
 		width: 200,
 		height: 200,
-		backgroundColor: 'transparent'
+		borderRadius: 30,
+		borderWidth: 2,
+		borderColor: GLOBAL_STYLES.highlightBlue,
+		backgroundColor: 'transparent',
+		justifyContent: 'center',
+		alignItems: 'center'
 	}
 });
