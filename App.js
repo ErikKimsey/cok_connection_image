a43da9bac7659a520504d92b0c2559aec4c06110
Erik Kimsey <kimseyerik@gmail.com>
2019-08-02 08:58:32 -0400

    Wrote out features in comments in App.js

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'native-base';
+
+/**
+ * Connection edit/view screen:
+ * /
+ * /
+ * -- "connection image"
+ * /
+ * -- Exit btn (x)
+ * /
+ * -- "Info":
+ * -- ind. inputs: first, last name, gender, bday (calendar), deceased (checkbox)
+ * /
+ * -- "Conatct Details"
+ * -- residence, tele#, email, job title, employer, salary range (dropdown)
+ * /
+ * -- "Social Media":
+ * -- fb, linkedin, twitter
+ * /
+ * --"Background":
+ * -- Browse (link to list?), Review (dropdown)
+ * /
+ * --"Highlights":
+ * -- textview box
+ * /
+ * "Save"
+ * -- btn
+ * /
+ */
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.js to start working on your app!</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
--- styles/global_styles.js
+++ styles/global_styles.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { Button } from 'native-base';
 
 export default (GLOBAL_STYLES = {
 	textColor: '#000000',
