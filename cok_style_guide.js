42902a16c7bcc337fd45658282bdef5a3716fcf4
Erik Kimsey <kimseyerik@gmail.com>
2019-08-02 08:44:10 -0400

    First commit. created global styles file

---
+++ cok_style_guide.js
@@ -0,0 +1,10 @@
+/**
+Background: white
+Text Color: Black
+Button and link highlight Color: rgb(80,141,179)
+Font: Furtura Light
+	A found Download Link: 
+		http://fontsgeek.com/fonts/Futura-Light
+	EOT Link (not used):
+		https://static.parastorage.com/services/third-party/fonts/user-site-fonts/fonts/2b40e918-d269-4fd9-a572-19f5fec7cd7f.eot?#iefix
+ */
---
+++ styles/global_styles.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default (GLOBAL_STYLES = {
+	textColor: '#000000',
+	highlightBlue: 'rgb(80,141,179)',
+	backgroundColor: '#ffffff'
+});
